f34f7db8d06c84cc5e6d253f463f77884c77a4f0
linux <zhengbo@ainirobot.com>
2018-06-22 03:00:29 -0700

    first commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 22 Jun 2018 02:52:10 AM PDT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world"<<endl;
+    return 0;
+}
